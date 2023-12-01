f0c49172ebee5dc68c0dc2df01a884ebf995fbf8
Favour Nzereogu <nzereogufavour2003@gmail.com>
2023-12-01 10:14:45 +0100

    Fixed the menu

---
+++ src/views/apps/user/list/AddUserDrawer.js
@@ -0,0 +1,171 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** MUI Imports
+import Drawer from '@mui/material/Drawer'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import { styled } from '@mui/material/styles'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+// ** Custom Component Import
+import CustomTextField from 'src/@core/components/mui/text-field'
+import StepperCustomHorizontal from 'src/views/forms/form-wizard/StepperCustomHorizontal'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Store Imports
+import { useDispatch, useSelector } from 'react-redux'
+
+// ** Actions Imports
+import { addUser } from 'src/store/apps/user'
+import { Dialog } from '@mui/material'
+
+const showErrors = (field, valueLen, min) => {
+  if (valueLen === 0) {
+    return `${field} field is required`
+  } else if (valueLen > 0 && valueLen < min) {
+    return `${field} must be at least ${min} characters`
+  } else {
+    return ''
+  }
+}
+
+const Header = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(6),
+  justifyContent: 'space-between'
+}))
+
+const schema = yup.object().shape({
+  company: yup.string().required(),
+  billing: yup.string().required(),
+  country: yup.string().required(),
+  email: yup.string().email().required(),
+  contact: yup
+    .number()
+    .typeError('Contact Number field is required')
+    .min(10, obj => showErrors('Contact Number', obj.value.length, obj.min))
+    .required(),
+  fullName: yup
+    .string()
+    .min(3, obj => showErrors('First Name', obj.value.length, obj.min))
+    .required(),
+  username: yup
+    .string()
+    .min(3, obj => showErrors('Username', obj.value.length, obj.min))
+    .required()
+})
+
+const defaultValues = {
+  email: '',
+  company: '',
+  country: '',
+  billing: '',
+  fullName: '',
+  username: '',
+  contact: Number('')
+}
+
+const SidebarAddUser = props => {
+  // ** Props
+  const { open, toggle } = props
+
+  // ** State
+  const [plan, setPlan] = useState('basic')
+  const [role, setRole] = useState('subscriber')
+
+  // ** Hooks
+  const dispatch = useDispatch()
+  const store = useSelector(state => state.user)
+
+  const {
+    reset,
+    control,
+    setValue,
+    setError,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = data => {
+    if (store.allData.some(u => u.email === data.email || u.username === data.username)) {
+      store.allData.forEach(u => {
+        if (u.email === data.email) {
+          setError('email', {
+            message: 'Email already exists!'
+          })
+        }
+        if (u.username === data.username) {
+          setError('username', {
+            message: 'Username already exists!'
+          })
+        }
+      })
+    } else {
+      dispatch(addUser({ ...data, role, currentPlan: plan }))
+      toggle()
+      reset()
+    }
+  }
+
+  const handleClose = () => {
+    setPlan('basic')
+    setRole('subscriber')
+    setValue('contact', Number(''))
+    toggle()
+    reset()
+  }
+
+  return (
+    <Dialog
+      open={open}
+      variant='temporary'
+      onClose={handleClose}
+      ModalProps={{ keepMounted: true }}
+      sx={{ '& .MuiDrawer-paper': { width: { xs: 100, sm: 400 } } }}
+
+      // sx={{ '& .MuiDrawer-paper': { overflow: 'visible' } }}
+    >
+      <Header>
+        <Typography variant='h5'>Add Staff</Typography>
+        <IconButton
+          size='small'
+          onClick={handleClose}
+          sx={{
+            p: '0.438rem',
+            borderRadius: 1,
+            color: 'text.primary',
+            backgroundColor: 'action.selected',
+            '&:hover': {
+              backgroundColor: theme => `rgba(${theme.palette.customColors.main}, 0.16)`
+            }
+          }}
+        >
+          <Icon icon='tabler:x' fontSize='1.125rem' />
+        </IconButton>
+      </Header>
+      <Box sx={{ p: theme => theme.spacing(0, 6, 6) }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* <StepperLinearWithValidation /> */}
+          <StepperCustomHorizontal />
+        </form>
+      </Box>
+    </Dialog>
+  )
+}
+
+export default SidebarAddUser
